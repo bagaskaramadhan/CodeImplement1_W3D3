6165278ccb01e5c9b971770e0973d94be7ddec93
Bagaskaramadhan <bagaskaramadhan@gmail.com>
2020-08-20 08:51:54 +0700

    Revisi

--- task3.js
+++ task3.js
@@ -2,7 +2,7 @@
 // angka 0 dalam variable adalah pemisah antara bilangan lainnya
 
 divideAndSort = (masukanAngka) => {
-    const a = masukanAngka.toFixed().split('0')
+    const a = masukanAngka.toString().split('0')
     // console.log(a)
     const b = a.map(e => e.split('').sort().join(''))
     // console.log(b)
